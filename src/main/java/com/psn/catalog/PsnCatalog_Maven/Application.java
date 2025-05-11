082b81ced12aa87858a5c59b37c3ee22b1f4f387
vinicius-robles <vinicius.robles@gmail.com>
2025-05-11 00:12:21 -0300

    "hotfix" alteração que resolve problema de CORS no Swagger UI

--- src/main/java/com/psn/catalog/PsnCatalog_Maven/Application.java
+++ src/main/java/com/psn/catalog/PsnCatalog_Maven/Application.java
@@ -1,7 +1,6 @@
 package com.psn.catalog.PsnCatalog_Maven;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
