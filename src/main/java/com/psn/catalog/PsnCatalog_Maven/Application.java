da013aa1f2bb8bfc77fe951e61d7c3ebd971d401
vinicius-robles <vinicius.robles@gmail.com>
2025-05-11 00:27:16 -0300

    Tentativa de evitar que a build crashe em ambiente de nuvem

--- src/main/java/com/psn/catalog/PsnCatalog_Maven/Application.java
+++ src/main/java/com/psn/catalog/PsnCatalog_Maven/Application.java
@@ -1,12 +1,20 @@
 package com.psn.catalog.PsnCatalog_Maven;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
+//
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(servers = { @Server(url = "/", description = ("Default Server URL"))})
+@OpenAPIDefinition(info = @Info(
+		title = "PSN Catalog API",
+		version = "1.0",
+		description = "Catálogo de jogos PlayStation"
+))
+
+//		servers = { @Server(url = "/", description = ("Default Server URL"))}
+
 public class Application {
 
 	public static void main(String[] args) {
